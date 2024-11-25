b370209a66b8e53bb41e3272902d706ac17986cb
Daniel Cavalcante <189470363+danielcavz@users.noreply.github.com>
2024-11-25 13:53:24 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para começar e me aprofundar em programação
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Você é a minha inspiração para conquistar tudo que sempre quis. Amo-te. ❤️</h1>
+  return (
+    <h1>
+      Você é a minha inspiração para conquistar tudo que sempre quis. Amo-te. ❤️
+    </h1>
+  );
 }
 
 export default Home;
